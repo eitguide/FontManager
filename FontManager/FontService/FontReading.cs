e116e6224f93e2cc3a9c4773f49856cb4ccf2dba
eitguide <eitguide.np@gmail.com>
2016-12-15 23:56:06 +0700

    add try catch to handle error

--- FontManager/FontService/FontReading.cs
+++ FontManager/FontService/FontReading.cs
@@ -25,114 +25,120 @@ namespace FontManager.FontService
             if (string.IsNullOrEmpty(filePath))
                 return null;
 
-            Face face = new Face(this.lib, filePath);
+            try {
+                Face face = new Face(this.lib, filePath);
 
-            if (face == null)
-                return null;
+                if (face == null)
+                    return null;
 
-           
-            fontInfo.Location = filePath;
-            fontInfo.FontFamily = face.FamilyName;
-            fontInfo.FontSubFamily = face.StyleName;
-            fontInfo.PostscriptName = face.GetPostscriptName();
 
-            fontInfo.GlyphCount = face.GlyphCount;
+                fontInfo.Location = filePath;
+                fontInfo.FontFamily = face.FamilyName;
+                fontInfo.FontSubFamily = face.StyleName;
+                fontInfo.PostscriptName = face.GetPostscriptName();
 
-            StringBuilder stringBuilder = new StringBuilder();
-            for (int i = 0; i < face.CharmapsCount; i++)
-            {
-                stringBuilder.Append(face.CharMaps[i].PlatformId);
+                fontInfo.GlyphCount = face.GlyphCount;
 
-                if (i != face.CharmapsCount - 1)
+                StringBuilder stringBuilder = new StringBuilder();
+                for (int i = 0; i < face.CharmapsCount; i++)
                 {
-                    stringBuilder.Append(", ");
-                }
-            }
+                    stringBuilder.Append(face.CharMaps[i].PlatformId);
 
-            fontInfo.Platform = stringBuilder.ToString();
-
-            OS2 os2 = face.GetSfntTable(SfntTag.OS2) as OS2;
-            if (os2 != null)
-                fontInfo.VendorID = System.Text.Encoding.UTF8.GetString(os2.VendorId);
-            else
-                fontInfo.VendorID = null;
-
-            int count = (int)face.GetSfntNameCount();
-            int curentNameIndex = -1;
-            // Utils.Logger.CodeIdsFont(face);
-            fontInfo.LanguageSupported = GetLanguageSupport(face);
-
-            fontInfo.StringLanguageSupported =  string.Join(", ", fontInfo.LanguageSupported.ToArray());
-
-            for (int i = 0; i < count; i++)
-            {
-                SfntName sfnt = face.GetSfntName((uint)i);
-                if (sfnt.PlatformId == PlatformId.Macintosh)
-                {
-                    if (sfnt.NameId < curentNameIndex)
-                        break;
-
-                    curentNameIndex++;
-                    switch (sfnt.NameId)
+                    if (i != face.CharmapsCount - 1)
                     {
-                        case NameTableId.TT_NAME_ID_COPYRIGHT:
-                            fontInfo.Copyright = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_FONT_FAMILY:
-                            fontInfo.FontFamily = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_FONT_SUBFAMILY:
-                            fontInfo.FontSubFamily = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_UNIQUE_ID:
-                            fontInfo.UniqueId = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_FULL_NAME:
-                            fontInfo.FullName = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_VERSION_STRING:
-                            fontInfo.Version = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_PS_NAME:
-                            fontInfo.PostscriptName = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_TRADEMARK:
-                            fontInfo.TradeMark = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_MANUFACTURER:
-                            fontInfo.Manufacturer = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_DESIGNER:
-                            fontInfo.Designer = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_DESCRIPTION:
-                            fontInfo.Description = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_VENDOR_URL:
-                            fontInfo.VendorURL = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_DESIGNER_URL:
-                            fontInfo.DesignerURL = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_LICENSE:
-                            fontInfo.License = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_LICENSE_URL:
-                            fontInfo.LicenseURL = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_PREFERRED_FAMILY:
-                            fontInfo.PreferredFamily = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_PREFERRED_SUBFAMILY:
-                            fontInfo.PreferredSubFamily = sfnt.StringAnsi;
-                            break;
-                        case NameTableId.TT_NAME_ID_MAC_FULL_NAME:
-                            fontInfo.MacFullName = sfnt.StringAnsi;
-                            break;
-                        default:
-                            break;
+                        stringBuilder.Append(", ");
                     }
                 }
+
+                fontInfo.Platform = stringBuilder.ToString();
+
+                OS2 os2 = face.GetSfntTable(SfntTag.OS2) as OS2;
+                if (os2 != null)
+                    fontInfo.VendorID = System.Text.Encoding.UTF8.GetString(os2.VendorId);
+                else
+                    fontInfo.VendorID = null;
+
+                int count = (int)face.GetSfntNameCount();
+                int curentNameIndex = -1;
+                // Utils.Logger.CodeIdsFont(face);
+                fontInfo.LanguageSupported = GetLanguageSupport(face);
+
+                fontInfo.StringLanguageSupported = string.Join(", ", fontInfo.LanguageSupported.ToArray());
+
+                for (int i = 0; i < count; i++)
+                {
+                    SfntName sfnt = face.GetSfntName((uint)i);
+                    if (sfnt.PlatformId == PlatformId.Macintosh)
+                    {
+                        if (sfnt.NameId < curentNameIndex)
+                            break;
+
+                        curentNameIndex++;
+                        switch (sfnt.NameId)
+                        {
+                            case NameTableId.TT_NAME_ID_COPYRIGHT:
+                                fontInfo.Copyright = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_FONT_FAMILY:
+                                fontInfo.FontFamily = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_FONT_SUBFAMILY:
+                                fontInfo.FontSubFamily = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_UNIQUE_ID:
+                                fontInfo.UniqueId = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_FULL_NAME:
+                                fontInfo.FullName = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_VERSION_STRING:
+                                fontInfo.Version = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_PS_NAME:
+                                fontInfo.PostscriptName = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_TRADEMARK:
+                                fontInfo.TradeMark = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_MANUFACTURER:
+                                fontInfo.Manufacturer = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_DESIGNER:
+                                fontInfo.Designer = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_DESCRIPTION:
+                                fontInfo.Description = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_VENDOR_URL:
+                                fontInfo.VendorURL = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_DESIGNER_URL:
+                                fontInfo.DesignerURL = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_LICENSE:
+                                fontInfo.License = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_LICENSE_URL:
+                                fontInfo.LicenseURL = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_PREFERRED_FAMILY:
+                                fontInfo.PreferredFamily = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_PREFERRED_SUBFAMILY:
+                                fontInfo.PreferredSubFamily = sfnt.StringAnsi;
+                                break;
+                            case NameTableId.TT_NAME_ID_MAC_FULL_NAME:
+                                fontInfo.MacFullName = sfnt.StringAnsi;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
 
             return fontInfo;
